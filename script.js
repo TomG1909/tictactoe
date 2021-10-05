109188bced812960eb067a280cd087f527dc0aa5
Toma Gela <toma.gela@gmail.com>
2021-10-05 23:21:32 +0200

    Final version

--- script.js
+++ script.js
@@ -41,61 +41,100 @@ function draw() {
 
 
 function checkForWin() {
+
     let winner;
 
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
 
-        document.getElementById('line-1').style.transform = 'scaleX(1.2)';
+        document.getElementById('line-1').style.transform = 'scaleX(1.0)';
+        document.getElementById('line-1').classList.remove('d-none');
     }
 
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
 
-        document.getElementById('line-2').style.transform = 'scaleX(1.2)';
+        document.getElementById('line-2').style.transform = 'scaleX(1.0)';
+        document.getElementById('line-2').classList.remove('d-none');
     }
 
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
 
-        document.getElementById('line-3').style.transform = 'scaleX(1.2)';
+        document.getElementById('line-3').style.transform = 'scaleX(1.0)';
+        document.getElementById('line-3').classList.remove('d-none');
+    }
+
+
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+        winner = fields[0];
+
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.0)';
+        document.getElementById('line-4').classList.remove('d-none');
     }
 
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
 
-        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.2)';
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.0)';
+        document.getElementById('line-5').classList.remove('d-none');
     }
-
-    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
-        winner = fields[0];
-
-        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.2)';
-    }
-
-
 
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
 
         document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1.0)';
+        document.getElementById('line-6').classList.remove('d-none');
     }
 
 
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
 
-        document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1.0)'
+        document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1.3)';
+        document.getElementById('line-7').classList.remove('d-none');
     }
 
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
 
-        document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1.0)'
+        document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1.3)';
+        document.getElementById('line-8').classList.remove('d-none');
     }
     if (winner) {
         console.log('Gewonnen!!', winner);
         gameOver = true;
+        setTimeout(function() {
+            document.getElementById('gameover').classList.remove('d-none');
+            document.getElementById('div-gameover').classList.add('bg-dark');
+            document.getElementById('btn').classList.remove('d-none');
+            document.getElementById('btn-pos').classList.add('d-flex');
+            document.getElementById('game').classList.add('d-none');
+            document.getElementById('player-panel').classList.add('d-none');
+        }, 2000);
+    }
+
+}
+
+function restart() {
+    gameOver = false;
+    fields = [];
+    document.getElementById('gameover').classList.add('d-none');
+    document.getElementById('btn').classList.add('d-none');
+    document.getElementById('div-gameover').classList.remove('bg-dark');
+    document.getElementById('game').classList.remove('d-none');
+    document.getElementById('player-panel').classList.remove('d-none');
+
+    for (let i = 1; i < 9; i++) {
+        document.getElementById('line-' + i).classList.add('d-none')
+
+    }
+
+    for (let i = 0; i < 9; i++) {
+        document.getElementById('circle_' + i).classList.add('d-none');
+        document.getElementById('cross_' + i).classList.add('d-none');
+
+
     }
 
 }
